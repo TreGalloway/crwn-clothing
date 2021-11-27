8a3e72dcd7a1cf46842fe7608315acf98cac497e
Tre' Galloway <tre@Tres-MacBook-Air.local>
2021-11-27 13:49:53 -0600

    added directory and Menu-item components and styles

---
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Homepage.styles.scss";
+import Directory from "../../Components/Directory/Directory.component";
+
+const Homepage = () => (
+    <div className='homepage'>
+     <Directory/>
+    </div>
+)
+export default Homepage;
