d3adfef1ac883cee1542e9044a625d0d140cefa5
Tre <tre@Tres-MacBook-Air.local>
2021-12-06 09:36:42 -0600

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header.component.jsx';
@@ -7,43 +7,55 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import SignIn from './components/sign-in/sign-in.component'; 
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
-import { auth } from './firebase/firebase.utils';
+import { userAuth , createUserProfileDocument} from './firebase/firebase.utils';
 
-class App extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      currentUser: null
-    };
-  }
+class App extends Component {
+  state = {
+    currentUser: null,
+  };
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = userAuth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const { userRef, onSnapshot } = await createUserProfileDocument(
+          userAuth
+        );
 
-      console.log(user);
+        onSnapshot(userRef, (snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+   this.unsubscribeFromAuth();
   }
 
   render() {
     return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignIn} />
-        </Switch>
-        </Router>
-      </div>
+      <Router>
+        <div>
+          <Header currentUser={this.state.currentUser} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignIn} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+//prettier-ignore
+import { getFirestore, doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
 
 const config = {
   apiKey: "AIzaSyB_k3MA3kfnToZOl_stqKj1EF2JXQ7erJU",
@@ -11,13 +17,46 @@ const config = {
   appId: "1:972792489554:web:8f4fd65019ab83a906a18b",
   measurementId: "G-XR5E7SRWX4"
 };
-const app = initializeApp(config);
 
-export const auth = getAuth(app);
+const app = initializeApp(config);
+const db = getFirestore(app);
+
+export const {auth,userAuth} = getAuth(app);
 export const firestore = getFirestore(app);
 
+export const createAccount = createUserWithEmailAndPassword;
 const provider = new GoogleAuthProvider();
+
 provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () =>
-  signInWithPopup(auth, provider).catch((error) => console.log(error));
+  signInWithPopup(userAuth, provider).catch((error) => console.log(error));
+
+  //Storing Data in Firebase
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = doc(db, "users", `${userAuth.uid}`);
+  const snapShot = await getDoc(userRef);
+
+  if (!snapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await setDoc(userRef, {
+        displayName,
+        email,
+        createAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log("error creating user", err.message);
+    }
+  }
+
+  return {
+    userRef,
+    onSnapshot,
+  };
+};
