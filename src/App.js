1b3b86a32b014097f58074ca8aa04b8aa2ae8f19
Tre <tre@Tres-MacBook-Air.local>
2021-12-18 16:26:58 -0600

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
+
 import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import { auth , createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { connect } from 'react-redux';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
