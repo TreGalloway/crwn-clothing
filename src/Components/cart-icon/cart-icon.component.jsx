0d717ce29bfa922e55df174f2235431aa7103589
Tre <tre@Tres-MacBook-Air.local>
2021-12-18 12:59:07 -0600

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/Components/cart-icon/cart-icon.component.jsx
+++ src/Components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.action'; 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import './cart-icon.styles.scss';
+import '../cart-item/cart-item.component'
 
 const CartIcon = ({ toggleCartHidden }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
