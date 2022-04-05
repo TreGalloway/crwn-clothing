eb0435399f2838eecfdd465bf779b7144d01b4d1
Tre <tre@Tres-MacBook-Air.local>
2022-04-05 16:04:33 -0500

    reset back to previous commit

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from './collection-styles.styles';
+
+const CollectionItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const addItemClickHandler = (item) => dispatch(addItem(item));  const { name, price, imageUrl } = item;
+
+  return (
+       <CollectionItemContainer>
+      <BackgroundImage className='image' imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton onClick={() => addItemClickHandler(item)} inverted>
+        Add to cart
+      </AddButton>
+    </CollectionItemContainer>
+  );
+}
+
+export default CollectionItem;
